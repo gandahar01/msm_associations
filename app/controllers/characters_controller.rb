da71aa45f5d0d090e9f383e091d1b9a8da7383d6
Shem Boduch <pbboduch@chicagobooth.edu>
2017-07-31 04:14:24 +0000

    msm_assoc v8

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,6 +8,7 @@ class CharactersController < ApplicationController
   def show
     @character = Character.find(params[:id])
     @movie = Movie.new
+    @actor = Actor.new
     render("characters/show.html.erb")
   end
 
